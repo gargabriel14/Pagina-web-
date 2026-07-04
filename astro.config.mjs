31dc75b89e8202c957750fe3e78d895b1ec3aa96
gargabriel14 <gargabriel14@gmail.com>
2026-07-04 15:26:28 +0200

    lighthouse ataque

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: process.env.PUBLIC_SITE_URL ?? 'https://pagina-web-orcin.vercel.app',
   trailingSlash: 'always',
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
